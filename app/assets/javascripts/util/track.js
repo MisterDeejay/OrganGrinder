601c90a0901bb10003ec821f6f40f3319bafa58d
App Academy Student <student@appacademy.io>
2015-06-03 13:41:53 -0700

    start play notes function

--- app/assets/javascripts/util/track.js
+++ app/assets/javascripts/util/track.js
@@ -1,25 +1,37 @@
 window.Track = function(name, roll) {
-  this.name = name;
-  var _roll = [];
-  var _startTime = (new Date()).getTime();
+  this._name = name;
+  this._roll = roll;
+  this._recording = false;
+};
 
-  // record: function() {
-  //
-  // },
-  //
-  // stopRecording: function() {
-  //
-  // },
-
-  Track.prototype = {
-    addNotes: function(notes) {
-      if (_roll.length === 0) {
-        rollEl = { time: 0, notes: notes };
-      } else {
-        currentTime = (new Date()).getTime() - _startTime;
-        rollEl = { time: currentTime, notes: notes }
-      }
-      _roll.push(rollEl);
-    },
-  };
+Track.prototype = {
+  record: function() {
+    this._recording = true;
+    this._startTime = Date.now();
+  },
+  stop: function() {
+    this._recording = false;
+  },
+  save: function() {
+    var name = prompt("Please enter your track name");
+    if (name != null) {
+      this.name = name;
+      // next need to add a li to the track list which plays back this track
+    }
+  },
+  play: function() {
+    this._roll.forEach(function(el) {
+      debugger;
+      setTimeout(playNotes(el.notes), el.time);
+    });
+  },
+  playNotes: function(notes){
+    console.log("playing notes!");
+  },
+  addNotes: function(notes) {
+    if (this._recording === true) {
+      var rollEl = { time: (Date.now() - this._startTime), notes: notes }
+      this._roll.push(rollEl);
+    }
+  }
 };
